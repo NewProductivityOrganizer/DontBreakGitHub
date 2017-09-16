afe10bd233cdc6064184b2812d9e82b74c38a964
palesa <palesa@172.17.65.173>
2017-09-16 15:56:24 -0600

    commiting changes to my branch

--- NewProductivityOrganizer/src/graphicalUserInterface/GraphicalUserInterface.java
+++ NewProductivityOrganizer/src/graphicalUserInterface/GraphicalUserInterface.java
@@ -18,18 +18,27 @@ import java.awt.event.ActionListener;
 import javax.swing.JPasswordField;
 import java.awt.Color;
 import javax.swing.JButton;
+import javax.swing.JRadioButton;
 /**
- * graphical user interface for system
+ * graphical user interface for the system
+ * currently displays login screen at first run
+ * if logging in for the first time, create account button will call a method that has 
+ * as frame with option to create an account
  * @author palesa
  * */
 public class GraphicalUserInterface extends JFrame  {
 
 	private JPanel contentPane;
+	private JPanel accountPane;//panel for create account
+	private JPanel individualAccountPane;//pane for individual accounts
 	private JTextField username;
 	private JPasswordField passwordField;
 	private JButton btnLogin;
 	private JLabel lblPrompt;
 	private JButton btnCreateAccount;
+	private JButton btnProceed;
+	private JRadioButton supervisorButton;
+	private JRadioButton studentWorkerButton;
 
 	/**
 	 * Launch the application.
@@ -95,22 +104,135 @@ public class GraphicalUserInterface extends JFrame  {
 		
 		btnCreateAccount = new JButton("Create Account>>");
 		btnCreateAccount.setBounds(6, 343, 160, 29);
+		contentPane.add(btnCreateAccount);
 		
 		/**
 		 * ActionListener for the create account button*/
+		
 		btnCreateAccount.addActionListener(new ActionListener()
 		{
 			
 			public void actionPerformed(ActionEvent event)
 			{
-			btnLogin.setVisible(true);
+				btnLogin.setVisible(true);
+			  createAccount();//method to take to create account window
 	        }
 		});
 		
-		contentPane.add(btnCreateAccount);
+		
+		
 		
 		
 	}
+	
+	public void createAccount() {
+		setTitle("Productivity Incentivizer");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(200, 200,350, 400);
+		accountPane = new JPanel();
+		accountPane.setBackground(new Color(205, 133, 63));
+		accountPane.setForeground(Color.BLACK);
+		accountPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(accountPane);
+		accountPane.setLayout(null);
+		
+		
+		supervisorButton= new JRadioButton("Supervisor");
+		supervisorButton.setBounds(140, 148, 135, 32);
+		accountPane.add(supervisorButton);
+		
+		studentWorkerButton= new JRadioButton("Student Worker");
+		studentWorkerButton.setBounds(140, 207, 135, 32);
+		accountPane.add(studentWorkerButton);
+		
+		
+		JLabel lblRoleSelection = new JLabel("      Select your role below:");
+		lblRoleSelection.setBounds(6, 70, 338, 38);
+		lblRoleSelection.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 15));
+		accountPane.add(lblRoleSelection);
+		
+		
+		btnProceed = new JButton("Proceed>>");
+		btnProceed.setBounds(6, 343, 160, 29);
+		accountPane.add(btnProceed);
+		
+		/**
+		 * Action Listener for proceed button
+		 * FIX this
+		 * */
+		
+		btnProceed.addActionListener(new ActionListener()
+		{
+			
+			public void actionPerformed(ActionEvent event)
+			{
+				
+			  individualAccount();//method to take to create account window
+	        }
+		});
+		
+		
+		
+		
+	}
+	
+	
+	
+	public void individualAccount() {
+		setTitle("Create Account");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(200, 200,350, 400);
+		individualAccountPane = new JPanel();
+		individualAccountPane.setBackground(new Color(205, 133, 63));
+		individualAccountPane.setForeground(Color.BLACK);
+		individualAccountPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(individualAccountPane);
+		individualAccountPane.setLayout(null);
+		
+
+		JLabel lblRoleSelection = new JLabel("Enter details below");
+		lblRoleSelection.setBounds(6, 70, 338, 38);
+		lblRoleSelection.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 15));
+		individualAccountPane.add(lblRoleSelection);
+		
+		
+		JLabel lblUSERNAME = new JLabel("USERNAME:");
+		lblUSERNAME.setBounds(49, 156, 79, 16);
+		contentPane.add(lblUSERNAME);
+		
+		
+		username = new JTextField();
+		username.setBounds(140, 148, 135, 32);
+		individualAccountPane.add(username);
+		username.setColumns(10);
+		
+		
+		JLabel lblPASSWORD = new JLabel("PASSWORD:");
+		lblPASSWORD.setBounds(49, 212, 79, 16);
+		individualAccountPane.add(lblPASSWORD);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(140, 207, 135, 32);
+		passwordField.setEchoChar('*');
+		individualAccountPane.add(passwordField);
+		
+		
+		JLabel lblVerify = new JLabel("VERIFY PASSWORD:");
+		lblVerify .setBounds(49, 200, 79, 16);
+		individualAccountPane.add(lblVerify);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(140, 207, 135, 32);
+		passwordField.setEchoChar('*');
+		individualAccountPane.add(passwordField);
+		
+		
+		
+		
+		
+		
+	}
+	
 
 	
 }
