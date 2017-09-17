fea8b5b20b372001ad6a278e1cd54e5cf4212cfb
yin <yin@172.16.7.97>
2017-09-17 16:04:31 -0600

    Cleaned up main class

--- NewProductivityOrganizer/src/ProductivityIncentivizer.java
+++ NewProductivityOrganizer/src/ProductivityIncentivizer.java
@@ -96,124 +96,6 @@ public class ProductivityIncentivizer {
 			break;
 		}
 	}
-		
-
-	/**
-	 * This player menu allows a user to choose from a selection of options and then a decision is amde based off of that
-	 * @author jburge
-	 * */
-	//No user method
-	private static int playerMenu()
-	{
-		boolean valid = false;
-		Scanner input = new Scanner(System.in);
-		int selection = 1;
-		while (!valid)
-		{
-			System.out.println("/**************************************************************/");
-			System.out.println("\nChoose the badge you would like to apply for: ");
-			System.out.println("0: Upgration -- Upgraded firmware on a printer");
-			System.out.println("1: The Xander -- developed a mobile app for CC ");
-			System.out.println("2: Humanitarian -- develop set of cards for ITS: Cards Against Humanity");
-			System.out.println("3: Seven League Boots --Help someone in facilities or HVAC");
-			System.out.println("4: ITS Manager -- Talk to a sales venfor for at least 5 minutes");
-			System.out.println("5: New is better -- Upgrade the OS on a computer");
-			System.out.print("> ");
-			try {
-				selection = input.nextInt();
-
-				input.nextLine();
-				if ((selection >= 0) && (selection <= 5))
-				{
-					valid = true;
-				}
-			}
-			//this will catch the mismatch and prevent the error
-			catch(InputMismatchException ex)
-			{
-				//still need to gobble up the end of line
-				input.nextLine();
-			}
-			if (!valid)
-			{
-				System.out.println("Invalid entry -- enter a number between 0 and 5");
-			}
-		}
-		return selection;
-	}
-	
-	/**
-	* Manually create each badge until a way to do this using a database is found
-	* This is merely for the first iteration purpose
-	*/
-	//No use method
-	public void initializeBadges() {
-	    Badge updation = new Badge("Updation","Upgrade firmware on a printer");
-	    badges.add(updation);
-	    
-	    Badge theXander = new Badge("theXander","develop a mobile app for CC");
-	    
-	    badges.add(theXander);
-	    Badge humanitarian = new Badge("Humanitarian","develop a set of cards for ITS: Cards Against Humanity");
-	    
-	    badges.add(humanitarian);
-	    Badge sevenLeagueBoots = new Badge("Seven league boots","Help someone in facilities or HVAC");
-	    
-	     badges.add(sevenLeagueBoots);
-	    Badge itsManager = new Badge("ITS Manager","Talk to a sales vendor for at least 5 minutes");
-	    badges.add(itsManager);
-	    Badge newIsBetter = new Badge("New is Better","Upgrade the OS on a computer");
-	    badges.add(newIsBetter);
-	}
-	
-	/**
-	* Method responsible for calling the executeCommand() for the apply for badge command and returns results of that call
-	* 
-	*/
-	//No use method
-	public  void sendBadgeApplication() {
-		initializeBadges(); // initialize simulated badge values
-		String display = "";
-		int badgeSelection = playerMenu();
-		switch(badgeSelection) {
-		case UPDATION :
-			applyForBadge = new ApplyForBadgeCommand(badges.get(UPDATION));
-		
-			display = 	applyForBadge.executeCommand();
-			System.out.println(display);
-			break;
-		case THE_XANDER:
-			applyForBadge = new ApplyForBadgeCommand(badges.get(THE_XANDER));
-			applyForBadge.executeCommand();
-			display = 	applyForBadge.executeCommand();
-			System.out.println(display);
-			break;
-		case HUMANITARIAN:
-			applyForBadge = new ApplyForBadgeCommand(badges.get(HUMANITARIAN));
-			applyForBadge.executeCommand();
-			display = 	applyForBadge.executeCommand();
-			System.out.println(display);
-			break;
-		case SEVEN_LEAGUE_BOOTS : 
-			applyForBadge = new ApplyForBadgeCommand(badges.get(SEVEN_LEAGUE_BOOTS));
-			applyForBadge.executeCommand();
-			display = 	applyForBadge.executeCommand();
-			System.out.println(display);
-			break;
-		case  ITS_MANAGER : 
-			applyForBadge = new ApplyForBadgeCommand(badges.get(ITS_MANAGER));
-			applyForBadge.executeCommand();
-			display = 	applyForBadge.executeCommand();
-			System.out.println(display);
-			break;
-		case  NEW_IS_BETTER: 
-			applyForBadge = new ApplyForBadgeCommand(badges.get(NEW_IS_BETTER));
-			applyForBadge.executeCommand();
-			display = 	applyForBadge.executeCommand();
-			System.out.println(display);
-			break;
-		}
-	}
 
 	
 	/**
@@ -259,23 +141,6 @@ public class ProductivityIncentivizer {
 	 * @param password
 	 * @return boolean used for testing
 	 */
-	/*public boolean addAccount(String employeeName, String employeeType, String supervisorCode, String username, String password) {
-		AccountFactory accountFactory = new AccountFactory();
-		if (employeeType.equals("1")) {
-			accountFactory.getAccount(employeeType, employeeName, username, password);		
-			return true;
-		}
-		else if (employeeType.equals("2")){
-			if (supervisorCode.equals("123")) {
-				accountFactory.getAccount(employeeType, employeeName, username, password);
-				return true;
-			}
-			else { //invalid code input	
-				return false;
-			}
-		}
-		return false;
-	}*/
 	public boolean addAccount(String employeeName, String employeeType, String supervisorCode, String username, String password){
 	try (
 			// Step 1: Allocate a database "Connection" object
@@ -321,11 +186,9 @@ public class ProductivityIncentivizer {
 	public List<StudentWorker> getLeadingBoard() {
 		List<StudentWorker> studentWorkerList = new ArrayList<StudentWorker>();
 		try (
-				// Step 1: Allocate a database "Connection" object
 				Connection conn = DriverManager.getConnection(
 						"jdbc:mysql://localhost:" + PORT_NUMBER + "/ProductivityIncentivizerDatabase?user=root&password=root"); // MySQL
 
-				// Step 2: Allocate a "Statement" object in the Connection
 				Statement stmt = conn.createStatement();
 				) {
 			String getTopTen = "SELECT UserID, ActualName, Point FROM StudentWorkerInformation ORDER BY Point DESC LIMIT 10";
@@ -365,8 +228,6 @@ public class ProductivityIncentivizer {
 			System.out.println("\nChoose what to do: ");
 			System.out.println("1: Log In");
 			System.out.println("2: Create Account");
-			/*System.out.println("3: Display Leading Board");
-			System.out.println("4: Display Incomplete Badges");*/
 			System.out.print("> ");
 			try {
 				selection = startChoice.nextInt();
@@ -377,10 +238,8 @@ public class ProductivityIncentivizer {
 					valid = true;
 				}
 			}
-			//this will catch the mismatch and prevent the error
 			catch(InputMismatchException ex)
 			{
-				//still need to gobble up the end of line
 				startChoice.nextLine();
 			}
 			if (!valid)
@@ -391,68 +250,6 @@ public class ProductivityIncentivizer {
 		}
 		return selection;
 	}
-	
-
-	/*
-	 * Display uncompleted badge
-	public void DisplayUncompletedBadge() {
-		try (
-				// Step 1: Allocate a database "Connection" object
-				Connection conn = DriverManager.getConnection(
-						"jdbc:mysql://localhost:" + PORT_NUMBER + "/ProductivityIncentivizerDatabase?user=root&password=root"); // MySQL
-
-				// Step 2: Allocate a "Statement" object in the Connection
-				Statement stmt = conn.createStatement();
-				) {
-			String getUncompletedBadge = "SELECT BadgeID, BadgeName, BadgeDescription FROM Badge WHERE BadgeStatus = 'Uncompleted'";
-			ResultSet uncompletedBadges = stmt.executeQuery(getUncompletedBadge);
-			int badgeIndex = 0;
-			while (uncompletedBadges.next()) {
-				badgeIndex += 1;
-				int badgeID = uncompletedBadges.getInt(1);
-				String badgeName = uncompletedBadges.getString(2);
-				String badgeDescription = uncompletedBadges.getString(3);
-				System.out.println( "\n" + badgeIndex +". Badge ID: " + badgeID + "    Badge Name: " + badgeName + "\nBadge Description: " + badgeDescription);
-			}
-			BackToMain();
-			
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
-	}*/
-	
-	/*
-	 * The method letting user to get back to start menu
-	public void BackToMain() {
-		Scanner in = new Scanner(System.in);
-		boolean valid = false;
-		int selection = 1;
-		while (!valid)
-		{
-			System.out.println("\nType 0 to get back to Main Menu");
-			System.out.print("> ");
-			try {
-				selection = in.nextInt();
-
-				in.nextLine();
-				if (selection == 0)
-				{
-					valid = true;
-				}
-			}
-			//this will catch the mismatch and prevent the error
-			catch(InputMismatchException ex)
-			{
-				//still need to gobble up the end of line
-				in.nextLine();
-			}
-			if (!valid)
-			{
-				System.out.println("Invalid entry -- enter 0");
-			}
-		}
-		operation();
-	}*/
 	
 	/**
 	 * operator of the software
@@ -466,12 +263,6 @@ public class ProductivityIncentivizer {
 		case 2:
 			promptUserToCreateAccount();
 			break;
-/*		case 3:
-			checkLeadingBoard();
-			break;
-		case 4:
-			DisplayUncompletedBadge();
-			break;*/
 		}
 	}
 	
